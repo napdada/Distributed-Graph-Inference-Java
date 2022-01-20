8e5426dbed8a25eed7db5af5646dca6db3426c52
NAP <napda@qq.com>
2022-01-20 11:57:55 +0800

    feat：添加删除中间结果的 shell
    Spark 迭代过程中，中间计算结果存储在磁盘上容易导致空间不足，通过定时执行 shell 来释放磁盘；

---
+++ src/main/shell/remove.sh
@@ -0,0 +1,2 @@
+find /Users/panpan/Documents/Code/Java/spark/src/main/resources/checkpoint/ -name '*' -amin +5 -ls -exec rm {} \;
+find /private/var/folders/08/mr24w8fj415_bk_y8_ysvd8w0000gn/T/blockmgr-e4f0a03a-0551-4ccc-b084-47d84ac9a34a -name '*' -amin +10 -ls -exec rm {} \;
